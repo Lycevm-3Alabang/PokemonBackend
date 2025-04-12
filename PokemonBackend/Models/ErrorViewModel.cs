8bf59fdc0b214c3f3848df08e812aec97447f254
Ninism <nin.alamo@outlook.com>
2025-04-12 14:54:13 +0800

    Removed

--- PokemonBackend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokemonBackend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
